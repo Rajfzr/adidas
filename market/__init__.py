8492f3843fbcff23f8bca91d5fcb115c746c1e00
Rajfzr <rss354747@gmail.com>
2023-09-05 09:52:43 +0530

    uy

--- market/__init__.py
+++ market/__init__.py
@@ -12,7 +12,7 @@ import os
 
 # kdmf
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://tatadb_user:JHUDOpY0em1PswBxEv7uYxKLZHilmQaB@dpg-cicfqfd9aq03rjn5173g-a.oregon-postgres.render.com:5432/tatadb'
+app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://ad_e8lb_user:45fE7dwCPw9ZGFCUXRev3ewgWzun6Z61@dpg-cjra6q0jbais73bibk40-a.oregon-postgres.render.com:5432/ad_e8lb'
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://tatadb_user:JHUDOpY0em1PswBxEv7uYxKLZHilmQaB@dpg-cicfqfd9aq03rjn5173g-a.oregon-postgres.render.com:5432/tatadb (db connection)'
