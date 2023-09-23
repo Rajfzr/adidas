4144b261d0912b65b45d599830844b79753a0c96
Rajfzr <144082865+Rajfzr@users.noreply.github.com>
2023-09-24 02:50:34 +0530

    Update __init__.py

--- market/__init__.py
+++ market/__init__.py
@@ -14,8 +14,7 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://ad_e8lb_user:45fE7dwCPw9ZGFCUXRev3ewgWzun6Z61@dpg-cjra6q0jbais73bibk40-a.oregon-postgres.render.com:5432/ad_e8lb'
 
-app.config['SESSION_TYPE'] = 'filesystem'  # You can choose other storage options
-Session(app)
+
 # app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://tatadb_user:JHUDOpY0em1PswBxEv7uYxKLZHilmQaB@dpg-cicfqfd9aq03rjn5173g-a.oregon-postgres.render.com:5432/tatadb (db connection)'
 
 
