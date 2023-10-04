fe4622201c03d25519d1270507fb7399e3a24150
Rajfzr <rss354747@gmail.com>
2023-10-04 10:23:22 +0530

    fd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from market import app
 
 if __name__ == '__main__':
+    start_keep_alive_thread()
     app.run(debug=False)
--- market/__init__.py
+++ market/__init__.py
@@ -5,14 +5,21 @@ from flask_login import LoginManager
 from flask_login import LoginManager 
 from flask_cors import CORS
 from flask_session import Session
+from threading import Thread
 import psycopg2
 import secrets 
 import os
+import time
+
+
+
+
 
 
 # kdmf
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://ad_e8lb_user:45fE7dwCPw9ZGFCUXRev3ewgWzun6Z61@dpg-cjra6q0jbais73bibk40-a.oregon-postgres.render.com:5432/ad_e8lb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://tatadb_user:JHUDOpY0em1PswBxEv7uYxKLZHilmQaB@dpg-cicfqfd9aq03rjn5173g-a.oregon-postgres.render.com:5432/tatadb (db connection)'
@@ -26,5 +33,27 @@ db=SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 app.app_context().push()
+
+# keep_alive.py
+
+
+def keep_alive():
+    with app.app_context():
+        while True:
+            try:
+                db.session.execute("SELECT 1")  # A simple SQL query to keep the connection alive
+                db.session.commit()
+            except Exception as e:
+                print(f"Error executing keep-alive query: {str(e)}")
+
+            time.sleep(60)  # Sleep for 5 minutes (adjust as needed)
+
+# Create a function to start the keep-alive thread
+def start_keep_alive_thread():
+    keep_alive_thread = Thread(target=keep_alive)
+    keep_alive_thread.daemon = True
+    keep_alive_thread.start()
+
+
 
 from market import routes
